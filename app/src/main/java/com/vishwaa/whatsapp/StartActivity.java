3626fd1055159fa00db27fa42484157941266834
Vishwaa <vishwaa0724@gmail.com>
2020-12-24 00:06:00 +0530

    Initial commit

---
+++ app/src/main/java/com/vishwaa/whatsapp/StartActivity.java
@@ -0,0 +1,17 @@
+package com.vishwaa.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
